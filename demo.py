6358f1b413d39ca61c4d2296430cb6f765aa0e5b
haidequanbu <2605965683@qq.com>
2022-08-15 17:23:33 +0800

    新增官网所给的三个接口（查询接口，聚合统计接口）的查询

--- demo.py
+++ demo.py
@@ -1,6 +1,51 @@
 from fofa import Fofa
 
-if __name__=="__main__":
-    fofa=Fofa(email='XXXX',key='XXXXX')
-    res=fofa.get_data(query_str='XXXX')
-    print(res)
+if __name__=="__main1__":
+    # 对应邮箱和api_key（需要有查询的权限才能够请求接口）
+    fofa=Fofa(email='zhaohaiquan@whu.edu.cn',key='48b26055a9b322a816f357607867d2f5')
+
+    # fofa.get_data(self,query_str='',page=1,size=100,field='',full=False)
+    # fofa.get_data参数：
+    # query_str:输入到fofa引擎中的查询语句,必须要有（必须参数）
+    # page:是否翻页，默认第一页（非必需参数）
+    # size:每页查询数量，最大10000条/页
+    # field:默认host,ip,port,可参考官网给的附录
+    # full:默认一年内数据，设置维True是查询所有记录
+    res=fofa.get_data(query_str='app="APPNODE"')
+    if ('errmsg' in res):
+        print(res)
+    else:
+        data = dict(res)
+        print(data)
+        for tem in data['results']:
+            print(tem)
+
+if __name__=="__main1__":
+    # 对应邮箱和api_key（需要有查询的权限才能够请求接口）
+    fofa = Fofa(email='xxxx', key='xxxx')
+
+    # get_host(self,host='',detail=False)
+    # fofa.get_host参数：
+    # host:ip地址
+    # detail：是否显示端口详情
+    res = fofa.get_host(host='xxxxxx')
+    if ('errmsg' in res):
+        print(res)
+    else:
+        data=dict(res)
+        print(data)
+
+if __name__=="__main3__":
+    # 对应邮箱和api_key（需要有查询的权限才能够请求接口）
+    fofa = Fofa(email='xxxx', key='xxxx')
+
+    # get_stats(self,query_str='',field='')
+    # fofa.get_stats参数：
+    # query_str：查询语句
+    # field:默认host,ip,port,可参考官网给的附录
+    res = fofa.get_stats(query_str='app="APPNODE"')
+    if ('errmsg' in res):
+        print(res)
+    else:
+        data=dict(res)
+        print(data)
--- fofa.py
+++ fofa.py
@@ -6,6 +6,47 @@ class Fofa():
         self.email=email
         self.key=key
         self.search_url='https://fofa.info/api/v1/search/all'
+        self.user_info_url='https://fofa.info/api/v1/info/my'
+        self.host_url='https://fofa.info/api/v1/host/'
+        self.stats_url='https://fofa.info/api/v1/search/stats'
+        self.get_user_info()
+
+    def get_user_info(self):
+        params = {}
+        params['email'] = self.email
+        params['key'] = self.key
+        try:
+            res=requests.get(self.user_info_url,params=params).json()
+            data=dict(res)
+            print('Welcome to fofa,' + data['username']+'!')
+        except:
+            raise RuntimeError('http请求失败')
+
+    def get_host(self,host='',detail=False):
+        host_url=self.host_url+host
+        print(host_url)
+        params = {}
+        params['email'] = self.email
+        params['key'] = self.key
+        params['detail']=detail
+        try:
+            res=requests.get(host_url,params=params).json()
+        except:
+            raise RuntimeError('http请求失败')
+        return res
+
+    def get_stats(self,query_str='',field=''):
+        params = {}
+        params['email'] = self.email
+        params['key'] = self.key
+        qbase64 = base64.b64encode(query_str.encode())
+        params['qbase64'] = qbase64
+        params['field'] = field
+        try:
+            res=requests.get(self.stats_url,params=params).json()
+        except:
+            raise RuntimeError('http请求失败')
+        return res
 
     def get_data(self,query_str='',page=1,size=100,field='',full=False):
         params={}
@@ -13,13 +54,12 @@ class Fofa():
         params['key']=self.key
         qbase64=base64.b64encode(query_str.encode())
         params['qbase64']=qbase64
-        if(page!=1):
-            params['page']=page
-        if(size!=100):
-            params['size']=size
-        if(field!=''):
-            params['field']=field
-        if(full!=False):
-            params['full']=full
-        res=requests.get(self.search_url,params=params).json()
+        params['page']=page
+        params['size']=size
+        params['field']=field
+        params['full']=full
+        try:
+            res=requests.get(self.search_url,params=params).json()
+        except:
+            raise RuntimeError('http请求失败')
         return res
